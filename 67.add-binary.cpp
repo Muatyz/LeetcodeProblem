fc4ed4737df73f301e7c6eb31bcafc7b59576603
Muatyz <1367620453@qq.com>
2023-04-15 10:40:27 +0800

    updated on wsl-ubuntu-22.04

--- 67.add-binary.cpp
+++ 67.add-binary.cpp
@@ -6,18 +6,31 @@
 
 // @lc code=start
 #include <string>
-#include <unordered_map>
+#include <algorithm>
 using namespace std;
+
+//抄的题解，清楚使用carry的思想，但是距离实现有一定的距离。
+
+
 class Solution {
 public:
     string addBinary(string a, string b) {
-        unordered_map<char,int> Binary = {
-            {'1',1},
-            {'0',0}
-        };
-        string s;
+        string res;
+        int i = a.length() - 1;int j = b.length() - 1;
+        int carry = 0;
+
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a[i--] - '0';
+            if(j >= 0) sum += b[j--] - '0';
+            carry = sum > 1 ? 1 : 0;
+            res += to_string(sum % 2);
+        }
         
-        
+        if(carry) res += to_string(carry);
+        reverse(res.begin(),res.end());
+
+        return res;
     }
 };
 // @lc code=end
